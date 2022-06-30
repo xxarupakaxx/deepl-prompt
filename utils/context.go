a3620f4073d07f24e5ec40ca00b16006f07afee1
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-07-01 04:00:23 +0900

    認証キーの設定

--- utils/context.go
+++ utils/context.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 type MyCtx struct {
@@ -15,8 +16,32 @@ type MyCtx struct {
 }
 
 type langKey string
+type authKey string
 
 const lang langKey = "lang"
+const auth authKey = "auth"
+
+func (m *MyCtx) SetAuthKey(s string) {
+	arrs := strings.Split(s,":")
+	if len(arrs)!=2 {
+		fmt.Println("認証キーの形式が違います。")
+		return
+	}
+	if arrs[1] != "fx"  {
+		fmt.Println("認証キーの形式が違います。")
+		return
+	}
+	m.c = context.WithValue(m.c, auth, s)
+}
+
+func (m *MyCtx) GetAuthKey() (string, error) {
+	v, ok := m.c.Value(auth).(string)
+	if !ok {
+		return "", fmt.Errorf("failed to get authkey")
+	}
+
+	return v, nil
+}
 
 func (m *MyCtx) SetLanguage(s string) {
 	m.c = context.WithValue(m.c, lang, s)
