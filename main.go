fcede392146f964eb8ecfaba0c12e36adaf39aa6
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-07-01 02:43:37 +0900

    最初の説明

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/xxarupakaxx/deepl-prompt/utils"
 )
 
-
+const (
+	version = "1.0.0"
+)
 
 func main() {
 	p := utils.NewPrompt()
+
+	fmt.Printf("deepl-prompt %s\n", version)
+	fmt.Println("Please use `exit` or `Ctrl-D` to exit this program.")
 	p.Run()
 }
-
